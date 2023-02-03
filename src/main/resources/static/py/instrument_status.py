deba8d000f979dd9e1d66f77d4701404df6ad3a6
lifangchn <michaelli423@hotmail.com>
2023-02-03 14:09:51 +0800

    fix: 删除1.0版不需要的代码

--- src/main/resources/static/py/instrument_status.py
+++
@@ -1,8 +0,0 @@
-from miio.device import Device
-
-if __name__ == '__main__':
-    device = Device(ip="192.168.120.44", token="19bc2330c330c048b5721195523906d7")
-    # siid:11, piid:2 获取当前功率接口
-    power_status = device.send("get_properties", [{'did': 'MYDID', 'siid': 11, 'piid': 2}])
-    print(power_status[0].get('value'))
-
