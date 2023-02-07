223ae154fb043834a554d42d6f90c39801fa8d25
lifangchn <michaelli423@hotmail.com>
2023-02-07 09:33:22 +0800

    前端UI调整

--- src/main/java/com/agilent/cdsa/common/CodeListConstant.java
+++ src/main/java/com/agilent/cdsa/common/CodeListConstant.java
@@ -22,5 +22,27 @@ public class CodeListConstant {
     public static final String PROJECT_VIEW = "ProjectView";
     public static final String CREATOR_VIEW = "CreatorView";
 
+    /**
+     * 柱状图颜色池
+     */
     public static final String[] COLOR_LIST = {"#63B2EE", "#76DA91", "#F8CB7F", "#F89588", "#7CD6CF", "#9192AB", "#7898E1", "#EFA666", "#EDDD86", "#9987CE", "#63B2EE", "#76DA91"};
+
+    /*Not Connect：white；
+    Idle：Green；
+    Error：Red；
+    Pre-run：Purple；
+    Running：Blue；
+    Not Ready：Yellow；
+    Maintenance Due：Orange；
+    Sleep：Teal；*/
+
+    public static final String INSTRUMENT_STATE_NOT_CONNECT = "Not Connect";
+    public static final String INSTRUMENT_STATE_IDLE = "Idle";
+    public static final String INSTRUMENT_STATE_ERROR = "Error";
+    public static final String INSTRUMENT_STATE_PRE_RUN = "Pre-run";
+    public static final String INSTRUMENT_STATE_RUNNING = "Running";
+    public static final String INSTRUMENT_STATE_NOT_READY = "Not Ready";
+    public static final String INSTRUMENT_STATE_MAINTENANCE_DUE = "Maintenance Due";
+    public static final String INSTRUMENT_STATE_SLEEP = "Sleep";
+    public static final String INSTRUMENT_STATE_OFFLINE = "Offline";
 }
