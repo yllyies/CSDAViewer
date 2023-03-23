ea2dfff401996e65607f626cb20477e9a037d6eb
lifang <michaelli423@hotmail.com>
2023-03-23 23:52:14 +0800

    release fix1

--- src/main/java/com/agilent/cdsa/common/util/PythonUtil.java
+++ src/main/java/com/agilent/cdsa/common/util/PythonUtil.java
@@ -30,6 +30,8 @@ public class PythonUtil {
     public static final String PARAMNAME_XIAOMI_HUMITURE_TOKEN = "xiaomi.humiture.token";
     public static final String FILENAME_HUMITURE_PYTHON = "/static/py/humiture.py";
     public static final String PARAMNAME_XIAOMI_INFO_PROPERTIES = "application-xiaomi-info.properties";
+    public static final String FILENAME_INSTRUMENT_STATUS_PYTHON = "/static/py/instrument_status.py";
+    public static final String FILENAME_INSTRUMENT_LEVEL_PYTHON = "/static/py/instrument_level.py";
 
     /**
      * 获取温湿度计信息
@@ -44,27 +46,14 @@ public class PythonUtil {
         } catch (Exception e) {
             log.warn("获取温湿度计信息失败，请检查 application-xiaomi-info.properties 是否存在。");
             e.printStackTrace();
-//            throw new BusinessException("获取温湿度计信息失败，请检查 application-xiaomi-info.properties 是否存在。");
         }
-        try {
-            ClassPathResource classPathResource = new ClassPathResource(FILENAME_HUMITURE_PYTHON);
-
-            String[] args = new String[]{"python", classPathResource.getAbsolutePath(), properties.getProperty(PARAMNAME_XIAOMI_HUMITURE_IP),
-                    properties.getProperty(PARAMNAME_XIAOMI_HUMITURE_TOKEN), properties.getProperty(PARAMNAME_XIAOMI_HUMITURE_DIDLIST)};
-
-            Process proc = Runtime.getRuntime().exec(args);// 执行py文件
-            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                message.append(line);
-            }
-            in.close();
-            proc.waitFor();
-        } catch (Exception e) {
-            log.warn("获取温湿度计信息失败，请检查 humiture.py及配置的小米账号密码是否正确。");
-            e.printStackTrace();
-//            throw new BusinessException("获取温湿度计信息失败，请检查 humiture.py及配置的小米账号密码是否正确。");
+        if (properties == null) {
+            return new XiaomiHumitureDto("None", "None");
         }
+        ClassPathResource classPathResource = new ClassPathResource(FILENAME_HUMITURE_PYTHON);
+        String[] args = new String[]{"python", classPathResource.getAbsolutePath(), properties.getProperty(PARAMNAME_XIAOMI_HUMITURE_IP),
+                properties.getProperty(PARAMNAME_XIAOMI_HUMITURE_TOKEN), properties.getProperty(PARAMNAME_XIAOMI_HUMITURE_DIDLIST)};
+        PythonUtil.pythonExec(args, message);
         if (JSONUtil.isJsonObj(message.toString())) {
             JSONObject jsonObject = JSONUtil.parseObj(message.toString());
             String temperature = jsonObject.get("temperature", String.class, true);
@@ -140,7 +129,6 @@ public class PythonUtil {
                 try {
                     String line2 = null ;
                     while ((line2 = br2.readLine()) !=  null ) {
-                        message.append(line2);
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -163,8 +151,8 @@ public class PythonUtil {
                 log.info("调用脚本成功");
             }
         } catch (Exception ex){
-            log.warn("获取仪器功率信息失败，请检查python脚本是否正确。");
-            ex.printStackTrace();
+            log.warn("获取仪器功率信息失败，请检查python脚本是否正确。" + message.toString());
+            log.warn(ex.getLocalizedMessage());
         } finally {
             if (process != null) {
                 try {
@@ -187,23 +175,9 @@ public class PythonUtil {
      */
     public static String doGetInstrumentPowerLevel(List<Double> powerList) {
         StringBuilder message = new StringBuilder();
-        try {
-            ClassPathResource classPathResource = new ClassPathResource("/static/py/instrument_power.py");
-            String s1 = JSONUtil.parseArray(powerList).toString();
-            String[] args = new String[]{"python", classPathResource.getAbsolutePath(), JSONUtil.quote(s1)};
-            Process proc = Runtime.getRuntime().exec(args);// 执行py文件
-            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                message.append(line);
-            }
-            in.close();
-            proc.waitFor();
-        } catch (Exception e) {
-            log.warn("机器学习功率档位失败，请检查 instrument_power.py 是否正确。");
-            e.printStackTrace();
-//            throw new BusinessException("机器学习功率档位失败，请检查 instrument_power.py 是否正确。");
-        }
+        ClassPathResource classPathResource = new ClassPathResource(FILENAME_INSTRUMENT_LEVEL_PYTHON);
+        String[] args = new String[]{"python ", classPathResource.getAbsolutePath(), JSONUtil.quote( JSONUtil.parseArray(powerList).toString())};
+        PythonUtil.pythonExec(args, message);
         return message.toString();
     }
 }
